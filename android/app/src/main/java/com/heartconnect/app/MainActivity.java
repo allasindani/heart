d14473536ec90521dc1b7d25c699e697b1eac542
allasindani <alasindani2020@gmail.com>
2026-04-30 22:41:10 +0200

    feat: Implement back button navigation for WebView
    Add custom onBackPressed logic to handle navigation within the WebView. This allows users to go back in the app's web content when the back button is pressed, improving user experience and navigation flow.

--- android/app/src/main/java/com/heartconnect/app/MainActivity.java
+++ android/app/src/main/java/com/heartconnect/app/MainActivity.java
@@ -1,5 +1,22 @@
 package com.heartconnect.app;
 
+import android.os.Bundle;
+import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onBackPressed() {
+        WebView webView = (WebView) this.bridge.getWebView();
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
